7120afb182f8569aac5b025a9b41bcf4c127eb70
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2026-01-02 22:47:27 +0100

    Add constraints to `versions` table
    - Make grammar_id foreign key non-nullable
    - Add unique composite index on grammar_id and identifier
    - Update database structure to reflect migration changes

--- config/db/migrate/20251231115033_create_versions.rb
+++ config/db/migrate/20251231115033_create_versions.rb
@@ -4,11 +4,13 @@ ROM::SQL.migration do
   change do
     create_table :versions do
       primary_key :id
-      foreign_key :grammar_id, :grammars, on_delete: :cascade
+      foreign_key :grammar_id, :grammars, on_delete: :cascade, null: false
       column :identifier, :string, null: false, index: true
 
       column :created_at, :timestamp, null: false, default: Sequel::CURRENT_TIMESTAMP
       column :updated_at, :timestamp, null: false, default: Sequel::CURRENT_TIMESTAMP
+
+      index %i[grammar_id identifier], unique: true
     end
   end
 end
--- config/db/structure.sql
+++ config/db/structure.sql
@@ -10,12 +10,16 @@ CREATE TABLE `grammars`(
 CREATE UNIQUE INDEX `grammars_name_index` ON `grammars`(`name`);
 CREATE TABLE `versions`(
   `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `grammar_id` integer REFERENCES `grammars` ON DELETE CASCADE,
+  `grammar_id` integer NOT NULL REFERENCES `grammars` ON DELETE CASCADE,
   `identifier` string NOT NULL,
   `created_at` timestamp DEFAULT(datetime(CURRENT_TIMESTAMP, 'localtime')) NOT NULL,
   `updated_at` timestamp DEFAULT(datetime(CURRENT_TIMESTAMP, 'localtime')) NOT NULL
 );
 CREATE INDEX `versions_identifier_index` ON `versions`(`identifier`);
+CREATE UNIQUE INDEX `versions_grammar_id_identifier_index` ON `versions`(
+  `grammar_id`,
+  `identifier`
+);
 INSERT INTO schema_migrations (filename) VALUES
 ('20251222200437_create_grammars.rb'),
 ('20251231115033_create_versions.rb');
